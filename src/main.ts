c3675310e3cbfdcc16a5155e2f5066a8826bfc51
ParadisusParadoxum <dev@jannes.dev>
2024-06-18 20:18:04 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import './assets/index.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
+app.use(createPinia()).use(router).use(vuetify).mount('#app')
