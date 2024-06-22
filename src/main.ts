38ee6ca8894a5e3b41b853c36b11d347d6e3eac2
ParadisusParadoxum <dev@jannes.dev>
2024-06-23 00:45:21 +0200

    Adding vite-plugin-vuetify for treeshaking components

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,8 @@ import router from './router'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'dark'
-  }
-})
+const vuetify = createVuetify({ theme: { defaultTheme: 'dark' } })
 
 app.use(createPinia()).use(router).use(vuetify).mount('#app')
