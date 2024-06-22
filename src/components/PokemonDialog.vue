e7051bd00999209d2b5be2d55e4eb0697978c6ff
ParadisusParadoxum <dev@jannes.dev>
2024-06-23 00:11:04 +0200

    Enhacing README.md & adding Pokemon detail dialog

---
+++ src/components/PokemonDialog.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Pokemon } from '@/stores/types'
+
+const props = defineProps<{ pokemon: Pokemon | null }>()
+const emits = defineEmits<{ (event: 'close'): void }>()
+</script>
+
+<template>
+  <v-dialog width="auto">
+    <v-card max-width="400" :title="props.pokemon?.names[1].name">
+      <template v-slot:actions>
+        <v-btn class="ms-auto" text="Ok" @click="$emit('close')"></v-btn>
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import type { Pokemon } from '@/stores/types'
 import { ref, computed } from 'vue'
 import { usePokemonStore } from '@/stores/pokemon'
+import PokemonDialog from '@/components/PokemonDialog.vue'
 import PokemonList from '@/components/PokemonList.vue'
-import type { Pokemon } from '@/stores/types'
 
+const dialog = ref<boolean>(false)
 const pokemon = ref<Pokemon[]>(usePokemonStore().pokemon)
 const searchTerm = ref<string>('')
+const selectedPokemon = ref<Pokemon | null>(null)
+
+const openDialog = (pokemon: Pokemon) => {
+  selectedPokemon.value = pokemon
+  dialog.value = !dialog.value
+}
 
 const filteredPokemon = computed(() => {
   return pokemon.value.filter(
@@ -19,13 +27,14 @@ const filteredPokemon = computed(() => {
 
 <template>
   <main>
+    <PokemonDialog v-model="dialog" :pokemon="selectedPokemon" @close="dialog = !dialog" />
     <v-container class="mt-16" style="max-width: 50rem">
       <v-text-field
         prepend-inner-icon="mdi-magnify"
         v-model="searchTerm"
         placeholder="Search for a Pokémon..."
       ></v-text-field>
-      <PokemonList :pokemon="filteredPokemon" />
+      <PokemonList :pokemon="filteredPokemon" @open="openDialog" />
     </v-container>
   </main>
 </template>
