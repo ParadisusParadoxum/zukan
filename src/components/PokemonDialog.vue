59f4c4e10b5da318729062b3d106535f86ffe9d2
ParadisusParadoxum <dev@jannes.dev>
2024-07-02 18:12:18 +0200

    Adding shiny toggle by clicking on a pokemon in dialog

--- src/components/PokemonDialog.vue
+++ src/components/PokemonDialog.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
 import type { Pokemon } from '@/stores/types'
 import { getTypeColor } from '@/stores/utils'
+import { shallowRef } from 'vue'
 import TypeSvg from './TypeSvg.vue'
 
 const props = defineProps<{ pokemon: Pokemon | undefined }>()
 const emits = defineEmits<{ (event: 'close'): void }>()
+
+const shiny = shallowRef<boolean>(false)
+
+const closeEvent = () => {
+  emits('close')
+  shiny.value = false
+}
 </script>
 
 <template>
-  <v-dialog width="auto">
+  <v-dialog width="auto" @click:outside="closeEvent">
     <v-card>
       <v-card-item>
         <template v-slot:prepend>
-          <v-img width="100" :src="props.pokemon?.sprites[0].front_default"></v-img>
+          <div @click="shiny = !shiny">
+            <v-img width="100" v-if="shiny" :src="props.pokemon?.sprites[2].front_shiny" />
+            <v-img width="100" v-else :src="props.pokemon?.sprites[0].front_default" />
+          </div>
         </template>
         <template v-slot:title>
           {{ props.pokemon?.names[1].name }}
@@ -33,7 +44,7 @@ const emits = defineEmits<{ (event: 'close'): void }>()
         </v-chip>
       </v-card-item>
       <template v-slot:actions>
-        <v-btn text="Close" @click="$emit('close')"></v-btn>
+        <v-btn text="Close" @click="closeEvent"></v-btn>
       </template>
     </v-card>
   </v-dialog>
